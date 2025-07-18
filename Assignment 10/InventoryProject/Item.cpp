1776a57e236fe4300aae4f173e6a923a9e0b0110
penguinsurfer <george.kong40@gmail.com>
2025-07-17 22:22:29 -0700

    Assignment 10

---
+++ Assignment 10/InventoryProject/Item.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Item.h"
+
+Item::Item() {
+	name = "Unknown";
+	quantity = 0;
+}
+
+Item::Item(string itemName, int itemQty) {
+	name = itemName;
+	quantity = itemQty;
+}
+
+void Item::display() {
+	cout << "Item: " << name << ", Quantity: " << quantity << endl;
+}
